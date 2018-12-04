01ded4669bc2f244795d87fb0f1761de319e61ce
Zixiao Huang <zixiao@uw.edu>
2018-12-04 15:51:06 -0800

    grad with datafame show

--- zixiao/server.R
+++ zixiao/server.R
@@ -1,13 +1,9 @@
 
 # Define server logic required to draw a histogram
 library("shiny")
-library("lubridate")
 library("dplyr")
-library("stringr")
-library("R.utils")
 library("data.table")
 library("ggplot2")
-library("maps")
 
 my_server <- function(input, output) {
   getData <- reactive({
