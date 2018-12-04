f64be8994a2625542ecf8e72b246e57258dce4c6
KhaVNguyen <kha.vng@gmail.com>
2018-12-03 17:15:33 -0800

    Create table for all colleges in budget. Fixes #2, #3, #4

--- kha/netprices/ui.R
+++ kha/netprices/ui.R
@@ -1,28 +1,46 @@
 library(shiny)
-
+library(shinythemes)
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-  
+  theme = shinytheme("lumen"),
   # Application title
   titlePanel("Net Prices of Colleges in the United States"),
   
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
+  # Sidebar with a slider input for number of bins
+  fluidRow(
     sidebarPanel(
-      selectInput(inputId = "state",
-                  label = "State",
-                  choices = unique(state.name)),
-      sliderInput(inputId = "income",
-                  label = "Your Family's Annual Income in $",
-                  min = 0, 
-                  max = 200000, 
-                  value = 50000, 
-                  step = 10000)
+      selectInput(
+        inputId = "state",
+        label = "State",
+        choices = unique(state.name)
+      ),
+      sliderInput(
+        inputId = "income",
+        label = "Your Family's Annual Income in $",
+        min = 0,
+        max = 200000,
+        value = 50000,
+        step = 10000
+      )
     ),
-    
-    # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("netpricesplot")
+      htmlOutput("net_prices_title"),
+      plotOutput("net_prices_plot")
+    ),
+    sidebarPanel(
+      sliderInput(
+        inputId = "budget",
+        label = "Your Budget (in $)",
+        min = 0,
+        max = 75000,
+        value = 30000,
+        step = 1000
+      )
+    ),
+    mainPanel(
+      htmlOutput("colleges_in_budget_title"),
+      dataTableOutput("colleges_in_budget_table")
     )
   )
-))
+)
+)
